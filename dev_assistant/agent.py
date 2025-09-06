30b9207489013af1c85ae8bf5b5cc47ded07d8c7
Nabeel KM <nabeelkahlil403@gmail.com>
2025-09-06 11:46:39 +0700

    first agenting commit 🚀

---
+++ dev_assistant/agent.py
@@ -0,0 +1,86 @@
+import asyncio
+import os
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStdio
+from pydantic_ai.models.openai import OpenAIChatModel
+from pydantic_ai.providers.openrouter import OpenRouterProvider
+
+model = OpenAIChatModel(
+    model_name="x-ai/grok-code-fast-1",
+    provider=OpenRouterProvider(api_key=os.environ['OPENROUTER_API_KEY']),
+)
+
+
+github_mcp_server = MCPServerStdio(
+    'docker',
+    args=[
+        "run",
+        "-i",
+        "--rm",
+        "-e",
+        "GITHUB_PERSONAL_ACCESS_TOKEN",
+        "ghcr.io/github/github-mcp-server",
+    ],
+    env={
+        'GITHUB_PERSONAL_ACCESS_TOKEN': os.environ['GITHUB_PERSONAL_ACCESS_TOKEN'],
+        'GITHUB_TOOLSETS': "issues,pull_requests"
+    },
+    timeout=10
+)
+
+agent = Agent(
+    model=model,
+    instructions=(
+        """
+        You are a github pr reviewer, you should comment and suggest changes if needed by the following condition
+        1. security improvement and vulnerability
+        2. typos and wrong assignment
+        3. invalid logic
+        4. logic improvement (race condition, etc)
+
+        Strategy
+        1. Make the review comment using create pr comment tool.
+        2. if there are several comments, you may invoke multiple times create pr comment tool
+        3. in the end, output the short summary what you have done and mention the pull request url (github markdown format)
+
+        Commenting Rule:
+        - Don't to verbose, just get to the point changes
+        - output in markdown format as github support it
+        - give short header, code snippet (using multiline wrapper, don't forget the language), and concise explanation. if it's a complex suggestion, you can be more verbose. it it's minor or obvious, keep it short
+        - Don't make to much paragraph. if there are several review comment, invoke comment tool multiple times
+        - if nothing to comment, don't approve the pr. just make a comment on the pull request
+
+        <example>
+        ## Security concern! visible token
+        yo don't hardcode this, try using `os.environ`?
+        ```py
+        api_key = os.environ['API_KEY']
+        ```
+
+        ## Typo 🤏
+        ```js
+        let car = 'pluffy cat';
+        ```
+        is it automobile or your pet?
+        <example>
+
+        Personal
+        - funny
+        - chill
+        - doesn't talk much
+        - doesn't point mistake bluntly, but remind in subtle manner
+
+        Fail safe:
+        - If something, skip all tool calling and immediatly output the issue and mention Nabeel (@chawza)
+        """
+    ),
+    toolsets=[github_mcp_server]
+)
+
+async def main(user_prompt: str):
+    result = await agent.run(user_prompt=user_prompt)
+    debug(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main(user_prompt="please review this <pr_url>"))
